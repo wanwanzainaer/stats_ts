334cff6cdd7be1151886c2dfd68d06c8d22c78aa
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-14 16:43:11 -0800

    Add the CsvFileReader class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('First init');
+import { CsvFileReader } from './CsvFileReader';
+const reader = new CsvFileReader('football.csv');
+reader.read();
+// enum = enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+console.log(`Man United won ${manUnitedWins} games`);
