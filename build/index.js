e857d638d0f671db8c03d5678f9963193c58896e
shihchuan ho <shihchuanho@shihchuans-mbp.lan>
2020-11-17 13:00:27 -0800

    Composition better than inheritance

--- build/index.js
+++ build/index.js
@@ -1,21 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
+var Summary_1 = require("./Summary");
 //Create an object that satisfies the 'DataReader' interface
-var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 //Create an instance of MatchReader and pass in something satisfying the 'dataReader' interface
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
-        manUnitedWins++;
-    }
-}
-console.log("Man United won " + manUnitedWins + " games");
-// the comment for the github commit
+var summary = Summary_1.Summary.winsAnaysisWithHtmlReport('Man United');
+summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
 //Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 
 //Create an instance of MatchReader and pass in something satisfying the 'dataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-let manUnitedWins = 0;
+const summary = Summary.winsAnaysisWithHtmlReport('Man United');
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
-    manUnitedWins++;
-  }
-}
-console.log(`Man United won ${manUnitedWins} games`);
-
-// the comment for the github commit
+summary.buildAndPrintReport(matchReader.matches);
